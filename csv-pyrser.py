00e0a7a9bf8f1380b940fe91beb8e7f9b4b698c6
Rebecca Turner <637275@gmail.com>
2017-07-06 19:16:59 -0400

    gonna run it

--- csv-pyrser.py
+++ csv-pyrser.py
@@ -1,39 +1,118 @@
 class CSVParser:
     def __init__(self):
-        self.field_start = True
-        self.in_quotes = False
-        self.quote_seen = False
+        self.delimiter       = ','
+        self.quote           = '"'
+        self.crlf            = '\n\r'
+        self.tok             = ''
+        self.row_raw         = ''
+        self.field_start     = True
+        self.in_quotes       = False
+        self.quote_seen      = False
+        self.fields          = 0
+        self.crlflen         = 2
+        self.passedcrlfchars = 0
 
-    def parse(self, txt, delimiter=',', quote='"', crlf='\x0d\x0a'):
-        tok = ''
+    def reset_field(self):
+        self.tok             = ''
+        self.field_start     = True
+        self.in_quotes       = False
+        self.quote_seen      = False
+
+    def reset_row(self):
+        self.reset_field()
+        self.row_raw         = ''
+        self.passedcrlfchars = 0
+
+    def reset(self):
+        self.__init__()
+
+    def parse_quote(self, c):
+        if self.field_start:
+            # quoted field
+            self.in_quotes = True
+            self.field_start = False
+        elif self.quote_seen:
+            # literal quote
+            self.tok += c
+            self.quote_seen = False
+        elif self.in_quotes:
+            # field end or literal quote char
+            # deal with it next pass
+            self.quote_seen = True
+        else:
+            # oh no
+            # not in quotes --- invalid
+            raise ValueError(f'Quote character {quote} present in '
+                'un-quoted field. Wrap the field in quotes and '
+                'double the quote character to parse.')
+
+    def parse_row(self, txt):
         row = []
-        ret = []
         for c in txt:
             if c is delimiter and self.in_quotes is False:
-                row.append(tok)
-                tok = ''
-                self.field_start = True
-                self.in_quotes = False
-                if self.quote_seen:
-                    self.quote_seen = False
+                # field end
+                row.append(self.tok)
+                self.reset_field()
             elif c is quote:
-                if self.field_start:
-                    # quoted field
-                    self.in_quotes = True
-                elif self.quote_seen:
-                    # literal quote
-                    tok += c
-                    self.quote_seen = False
-                elif self.in_quotes:
-                    # field end or literal quote char
-                    # deal with it next pass
-                    self.quote_seen = True
-                else:
-                    # oh no
-                    # not in quotes
-                    raise ValueError(f'Quote character {quote} present in '
-                        'un-quoted field. Wrap the field in quotes and '
-                        'double the quote character to parse.')
+                self.parse_quote(c, kwargs)
             else:
                 self.field_start = False
-                tok += c
+                self.tok += c
+        return row
+
+    def parse(self, txt, delimiter = ',', quote = '"', crlf = '\r\n'):
+        self.delimiter = delimiter
+        self.quote     = quote
+        self.crlf      = crlf
+        ret            = []
+        row_number = 0
+        for c in txt:
+            if c is quote:
+                self.parse_quote(c)
+            elif c is delimiter and self.in_quotes is False:
+                if row_number is 0:
+                    self.fields += 1
+                self.field_start = True
+                self.row_raw += c
+            else:
+                if (self.in_quotes is False
+                        and c is crlf[self.passedcrlfchars]):
+                    self.passedcrlfchars += 1
+                    if self.passedcrlfchars is self.crlflen:
+                        # row end!
+                        ret.append(self.parse_row(self.row_raw))
+                        self.reset_row()
+                else:
+                    self.field_start = False
+                    self.row_raw += c
+        return ret
+
+import argparse
+
+csvparser = CSVParser()
+
+prog = 'csvparse'
+argparser = argparse.ArgumentParser(
+    description='Simple CSV Parser',
+    prog=prog)
+
+argparser.add_argument('src_file', nargs='*',
+    help='The filename of a CSV file or files to parse.')
+
+argparser.add_argument('-', action='store_true', dest='use_stdin',
+    help='Read from STDIN instead of a file.')
+
+args = argparser.parse_args()
+
+if args.use_stdin or args.src_file is None:
+    # catenate stdinput, parse / render
+    src = ''
+    for line in sys.stdin:
+        src += line + '\n'
+    csvparser.parse(src)
+    exit()
+
+for fname in args.src_file:
+    with open(fname, 'r', encoding='utf-8') as f:
+        csvparser.reset()
+        csvparser.parse(f.read())
